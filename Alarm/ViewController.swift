de30b077ee043a5e1fc94bddfca892502810df92
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-18 12:35:17 +0900

    Simple Alarm / 심플 알람

--- Alarm/ViewController.swift
+++ Alarm/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var alarmTime: UILabel!
+    @IBOutlet weak var time: UILabel!
+    var myTime = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime.text = formatter.string(from: myDatePicker.date)
+        
+        myTime = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTime) in self.updateTime()})
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        time.text = formatter.string(from: date)
+        
+        if time.text == alarmTime.text{
+            view.backgroundColor = UIColor.yellow
+        }
     }
-
-
+    
+    //날짜 선택
+    @IBAction func DatePickerValueChanged(_ sender: Any) {
+        //print("change Date Picker")
+        //print(myDatePicker.date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime.text = formatter.string(from: myDatePicker.date)
+    }
+    
+    //버튼 클릭시 배경화면이 흰색으로 바뀐다.
+        @IBAction func selectTime(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
 }
 
